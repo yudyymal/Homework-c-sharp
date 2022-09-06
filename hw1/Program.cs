034dbd51b630cc582577d926692829ef73d9ebac
Alexander Timirov <yudyymal@gmail.com>
2022-09-06 23:26:21 +0500

    ДЗ 1

--- hw1/Program.cs
+++ hw1/Program.cs
@@ -1,1 +1,23 @@
-﻿Console.WriteLine("Введите")
+﻿Console.WriteLine("Введите число 1");
+string a = Console.ReadLine();
+int numberA = Convert.ToInt32(a);
+Console.WriteLine("Введите число 2");
+string b = Console.ReadLine();
+int numberB = Convert.ToInt32(b);
+
+int max = numberA;
+int min = numberB;
+
+if (max == min)
+{
+    Console.WriteLine("Данные числа равны");
+}
+else if (numberB > max)
+{
+    max = numberB;
+    min = numberA;
+}
+if (max != min)
+{
+    Console.WriteLine("Наибольшее из данных чисел - число " + max + " Наименьшее - " + min);
+}
